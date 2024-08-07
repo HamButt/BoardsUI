2f2bf3a4ebbcfe187a977b6758cfc3c1bf3c8809
Hammad butt <98612242+HamButt@users.noreply.github.com>
2024-08-07 23:55:49 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,11 +5,11 @@ export function middleware(request) {
   const cookie = request.cookies.get('token');
   const url = request.url;
   
-  if (!cookie && url.includes('/boards/create')){
+  if (!cookie && url === 'https://praiseboard.vercel.app/boards/create)){
     return NextResponse.redirect('https://praiseboard.vercel.app/boards/login')
   }
   
-  if(cookie && url ===  'https://praiseboard.vercel.app/boards/login'){
+  if(cookie && url === 'https://praiseboard.vercel.app/boards/login'){
     return NextResponse.redirect('https://praiseboard.vercel.app/boards/user/dashboard')
   }
 
