1550433ad797533b09fb693a206813a4578d8ada
abc <MY_NAME@example.com>
2024-08-16 20:08:25 +0500

    ui fixed

--- pages/boards/dashboard.js
+++ pages/boards/dashboard.js
@@ -20,12 +20,14 @@ function Dashboard() {
     }
 
   return (
-    <div className=' mt-3' >
+    <div className='mt-3'>
+        
         <Head>
             <title>Dashboard</title>
         </Head>
 
-        <h1 className='ms-3 text-2xl font-mono font-semibold' >Dashboard</h1>
+        <h1 className='ms-3 text-2xl font-mono font-semibold'>Dashboard</h1>
+        
         <div className='flex items-center justify-center mt-3' >
 
             <div className=" w-full mx-5">
@@ -36,21 +38,27 @@ function Dashboard() {
                         <th>Occasions</th>
                     </tr>
                     </thead>
-                    {boards.length > 0 ? boards.map((board,index) => {
-                        return (
-                            <tbody key={index} >
-                                <tr>
+                    <tbody>
+                        {boards.length > 0 ? boards.map((board,index) => {
+                            return (
+                                <tr key={index}>
                                     <th>{board.totalBoards}</th>
                                     <th>{board.occasion}</th> 
                                 </tr>
-                            </tbody>
                             )
                         })
                     : 
-                        <div className=' flex items-center justify-center text-center ms-40 h-screen w-full'>
-                            <Loader color="#FF9669" size="lg" />
-                        </div>
+                    (
+                    <tr>
+                        <td colSpan="2">
+                            <div className='flex items-center justify-center text-center ms-40 w-full'>
+                                <Loader color="#FF9669" size="lg" />
+                            </div>
+                        </td>
+                    </tr>
+                )
                     } 
+                    </tbody>
                 </table>
             </div>
         </div>
